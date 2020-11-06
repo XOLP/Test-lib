ec6ed27704d6483c56e78248c8bc939db516b6eb
Gleb <gleb.fedchun@gmail.com>
2020-11-06 22:22:14 +0300

    Push

---
+++ Test/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using ds.test.impl;
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plugins a = new Plugins();
+            a.Info();
+            Console.WriteLine("Press enter to start");
+            while (1== 1)
+            {
+                
+                string check = Console.ReadLine();
+                
+                if (check == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Enter two numbers");
+                    try
+                    {
+                        
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        int y = Convert.ToInt32(Console.ReadLine());
+
+
+                        a.Run(x, y);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Incorrect format");
+
+                    }
+                }
+               
+                    
+               
+            }
+                    
+        }
+    }
+}
+
---
+++ lib/Plugins.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ds.test.impl
+{
+   
+    public class Plugins : PluginFactory
+    {
+        private static int PlugsCount = 1;
+        private static string vers = "1.0";
+        private static string name = "Calculator";
+        public string PluginName
+        {
+            
+            get => name;
+        }
+        public string Version
+        {
+            get => vers;
+        }
+        
+
+
+        public int PluginsCount
+        {
+            get { return PlugsCount; }
+        }
+        public string[] GetPluginNames { get; } = new string[] { };
+        
+        public IPlugin GetPlugin(string pluginName)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public void Info()
+        {
+            Console.WriteLine($"Name: {name}; Version: {vers}; PluginsCount: {PluginsCount}");
+        }
+
+        public int Run(int x , int y)
+        {
+            double result;
+            result = x * y;
+            Console.WriteLine(x + "*" + y + " = " + result);
+            result = x + y;
+            Console.WriteLine(x + "+" + y + " = " + result);
+            result = x - y;
+            Console.WriteLine(x + "-" + y + " = " + result);
+            result =  Math.Pow(x,y);
+            Console.WriteLine(x + "^" + y + " = " + result);
+            result = Math.Sqrt(x);
+            Console.WriteLine("sqrt(x) = " + result );
+            result = Math.Sqrt(y);
+            Console.WriteLine("sqrt(y) = " + result);
+            result = x + y;
+            return 1; 
+        }
+  
+    }
+    public interface IPlugin
+    {
+        string PluginName { get; set; }
+        string Version { get; set; }
+        System.Drawing.Image Image { get; }
+        string Description { get; }
+        int Run(int input1, int input2);
+    }
+
+
+}
